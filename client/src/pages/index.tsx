db5a8d4150dbdd5adb5f95ce09fc192c1a9d2d67
jimmy <jimmychen12138@hotmail.com>
2021-03-03 21:32:56 +1100

    Added icon

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { Typography } from "@material-ui/core";
 import { NavButtonLink } from "components/Common/Link";
 import { Layout } from "components/Layout/Layout";
 import { LOGIN_PATH, REGISTER_PATH } from "util/constants";
 
 const Index = () => (
   <Layout>
+    <img src="/favicon.ico" alt="logo" width={200} height={200} />
+    <Typography>Making YourUNSW a little bit less lonely~</Typography>
+    &nbsp;
     <NavButtonLink href={LOGIN_PATH} color="primary" variant="contained">
       Login
     </NavButtonLink>
