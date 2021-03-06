e1ba3770440a1fab75ce232d20fbec4dec034d6b
Braedon Wooding <braedonw@resonatesolutions.com.au>
2021-03-07 06:05:34 +1100

    Frontend fixes

--- client/src/components/Common/FormikUI.tsx
+++ client/src/components/Common/FormikUI.tsx
@@ -4,7 +4,7 @@ import {
   FormHelperText,
   makeStyles,
   OutlinedInputProps,
-  TextField,
+  TextField
 } from "@material-ui/core";
 import { FieldHookConfig, useField } from "formik";
 import { FC, ReactNode } from "react";
@@ -75,6 +75,61 @@ export const useStyles = makeStyles((theme) => ({
     boxShadow: "0 1px 2px rgba(0,0,0,0.2)",
     [theme.breakpoints.down("md")]: {
       width: "100%",
+    }
+  },
+  avatar: {
+    width: "200px",
+    height: "200px",
+    '&:hover': {
+      background: "black"
+    }
+  },
+  avatar_overaly_wrapper: {
+    "z-index": 10,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    background: "rgba(200,200,200,.9)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  avatar_root: {
+    "position": "relative",
+    cursor: "pointer"
+  },
+  avatar_overlay: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    borderRadius: "100px",
+    height: "100%",
+    width: "100%",
+    opacity: 0,
+    transition: ".2s ease",
+    "background-color": "rgba(0, 0, 0, 0.6)",
+    '& div': {
+      color: "white",
+      'font-size': "20px",
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      'text-align': 'center',
     },
+    '&:hover': {
+      opacity: 1,
+    }
+  },
+  profileName: {
+    textAlign: "center",
+  },
+  margin: {
+    margin: theme.spacing(1),
   },
 }));
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,17 +1,31 @@
 import { makeStyles } from "@material-ui/core";
 import { FC } from "react";
 
-export const Layout: FC = ({ children }) => {
+export const FullyCenteredLayout: FC = ({ children }) => {
   const classes = useStyles();
-  return <div className={classes.content}>{children}</div>;
+  return <div className={classes.fullyCenteredContent}>{children}</div>;
+};
+
+export const HorizontallyCenteredLayout: FC = ({ children }) => {
+  const classes = useStyles();
+  return <div className={classes.horizontallyCenteredContent}>{children}</div>;
 };
 
 const useStyles = makeStyles((theme) => ({
-  content: {
+  fullyCenteredContent: {
     display: "flex",
     minHeight: "100vh",
     flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
+    fontSize: "calc(10px + 2vmin)",
+    padding: theme.spacing(3),
+  },
+  horizontallyCenteredContent: {
+    display: "flex",
+    minHeight: "100vh",
+    flexDirection: "column",
+    justifyContent: "start",
     alignItems: "center",
     fontSize: "calc(10px + 2vmin)",
     padding: theme.spacing(3),
