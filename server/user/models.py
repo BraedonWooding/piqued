064b824d87d4a65bf6edd6143b9cd8a8093ff9c4
jimmy <jimmychen12138@hotmail.com>
2021-03-06 13:36:14 +1100

    Changed user schema

--- server/user/models.py
+++ server/user/models.py
@@ -7,7 +7,8 @@ class User(models.Model):
     first_name = models.TextField(max_length=20)
     last_name = models.TextField(max_length=20)
     email = models.EmailField()
-    date_of_birth = models.DateTimeField()
+    date_of_birth = models.DateField()
+    password = models.CharField(default=None, max_length=100)
 
     def _str_(self):
         return self.title
