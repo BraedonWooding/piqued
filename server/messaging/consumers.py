a4ac5a47d9e554cccb4e990ceee865e973a24030
Nicholas Thumiger <nicholasthumiger@gmail.com>
2021-03-07 00:05:34 +1100

    Implemented the storage of messages into the table storage
    The message history for a particular group is also loaded in upon the
    connection of the websocket.

--- server/messaging/consumers.py
+++ server/messaging/consumers.py
@@ -1,11 +1,20 @@
 # messaging/consumers.py
 import json
+from datetime import datetime, timedelta
+
+from asgiref.sync import sync_to_async
+from azure.cosmosdb.table.models import Entity
+from azure.cosmosdb.table.tableservice import TableService
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+account_name = "devstoreaccount1"
+account_key = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
+tab_name = "Messages"
 
 class GroupConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['group_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.group_name = self.scope['url_route']['kwargs']['group_name']
+        self.room_group_name = 'chat_%s' % self.group_name
 
         # Join room group
         await self.channel_layer.group_add(
@@ -13,7 +22,20 @@ class GroupConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        self.table_service = TableService(account_name=account_name, account_key=account_key, is_emulated=True)
+        
+        # This is the only synchronous call made in this consumer, and it runs upon connection request
+        # Performance should not suffer as a result
+        self.msgs = await sync_to_async(self.get_history)()
+
         await self.accept()
+
+        for msg in self.msgs:
+            await self.chat_message(msg)
+
+    def get_history(self, msgs_since = datetime.utcnow() - timedelta(days=30)):
+        msgs = self.table_service.query_entities(tab_name, filter="PartitionKey eq '" + str(self.group_name) + "'")
+        return msgs
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -27,20 +49,32 @@ class GroupConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        msg = {
+            #TODO: change the partition key to be the group ID. For now, i'm leaving it as whatever the string in the URI is 
+            'PartitionKey': str(self.group_name), 
+            'RowKey': str(int(datetime.utcnow().timestamp()*10000000)),
+            'MessageContents': message,
+            'Deleted': 0,
+            'UserId': 0,
+            'Assets': "",
+            'ModifiedAt': datetime.utcnow()}
+        self.table_service.insert_entity('Messages', msg)
+
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'MessageContents': message
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event['message']
+        message = event['MessageContents']
+        print(message)
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'MessageContents': message
         }))
