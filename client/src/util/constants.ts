fc9bff578f866c0d8679e72553a868ff04016eb0
jimmy <jimmychen12138@hotmail.com>
2021-03-06 21:58:32 +1100

    Added basic axios interceptor

--- client/src/util/constants.ts
+++ client/src/util/constants.ts
@@ -1,3 +1,4 @@
+export const isProd = process.env.NODE_ENV === "production";
 export const LOGIN_PATH = "/auth/login";
 export const REGISTER_PATH = "/auth/register";
 export const FORGOT_PASSWORD_PATH = "/auth/forgot-password";
