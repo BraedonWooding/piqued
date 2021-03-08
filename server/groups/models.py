7a3c52252384c9272fb1b74cc11b688858ca5ade
adamtemesvary <z5075774@student.unsw.edu.au>
2021-03-08 11:27:52 +1100

    consumer stubs

--- server/groups/models.py
+++ server/groups/models.py
@@ -7,38 +7,38 @@ from django.db.models.fields.related.ManyToMany import ManyToMany
 from ..user import models as PiquedUserModels
 # Create your models here.
 
-# class PiquedGroup(models.Model):
-#         # Foreign key for interests
+class PiquedGroup(models.Model):
+        # Foreign key for interests
 
-#         # group: OneToOneField = models.OneToOneField(Group) # users and permissions
+        group: OneToOneField = models.OneToOneField(Group) # users and permissions
 
-#         # users: ManyToMany = models.ManyToMany(PiquedUser) # foreign key??
-#         # # one to one relo, with main group
+        users: ManyToMany = models.ManyToMany(PiquedUser) # foreign key??
+        # one to one relo, with main group
 
-#         # # TODO add interests
-#         # # interests: ManyToMany = models.ManyToMany(InterestPlaceHolderClass, on_delete = models.CASCADE)
-#         # # TODO add RSS feeds
-#         # # rss_ids: ManyToMany??
+        # TODO add interests
+        # interests: ManyToMany = models.ManyToMany(InterestPlaceHolderClass, on_delete = models.CASCADE)
+        # TODO add RSS feeds
+        # rss_ids: ManyToMany??
         
-#         # is_course: BooleanField =  models.BooleanField(default = False)
-#         # DeletedAt: DateTimeField = models.DateTimeField(default = None, blank = True, null = True)
-#         # ModifiedAt: DateTimeField = models.DateTimeFied(auto_now = True)        # update on modification
-#         # CreatedAt: DateTimeField = models.DateTimeField(auto_now_add = True)    # update on creation
+        is_course: BooleanField =  models.BooleanField(default = False)
+        DeletedAt: DateTimeField = models.DateTimeField(default = None, blank = True, null = True)
+        ModifiedAt: DateTimeField = models.DateTimeFied(auto_now = True)        # update on modification
+        CreatedAt: DateTimeField = models.DateTimeField(auto_now_add = True)    # update on creation
 
-#         group = models.OneToOneField(Group) # users and permissions
+        # group = models.OneToOneField(Group) # users and permissions
 
-#         users = models.ManyToMany(PiquedUser) # foreign key??
-#         # one to one relo, with main group
+        # users = models.ManyToMany(PiquedUser) # foreign key??
+        # # one to one relo, with main group
 
-#         # TODO add interests
-#         # interests: ManyToMany = models.ManyToMany(InterestPlaceHolderClass, on_delete = models.CASCADE)
-#         # TODO add RSS feeds
-#         # rss_ids: ManyToMany??
+        # # TODO add interests
+        # # interests: ManyToMany = models.ManyToMany(InterestPlaceHolderClass, on_delete = models.CASCADE)
+        # # TODO add RSS feeds
+        # # rss_ids: ManyToMany??
         
-#         is_course = models.BooleanField(default = False)
-#         DeletedAt = models.DateTimeField(default = None, blank = True, null = True)
-#         ModifiedAt = models.DateTimeFied(auto_now = True)        # update on modification
-#         CreatedAt = models.DateTimeField(auto_now_add = True)    # update on creation
+        # is_course = models.BooleanField(default = False)
+        # DeletedAt = models.DateTimeField(default = None, blank = True, null = True)
+        # ModifiedAt = models.DateTimeFied(auto_now = True)        # update on modification
+        # CreatedAt = models.DateTimeField(auto_now_add = True)    # update on creation
 
 
         
