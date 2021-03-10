7f48ed8fb13c6a42ca096f9fdea65b75252a0b77
adamtemesvary <z5075774@student.unsw.edu.au>
2021-03-10 19:19:17 +1100

    uplift

--- server/groups/views.py
+++ server/groups/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .serializers import PiquedGroupSerializer
 # Create your views here.\
 
 def index(request):
     return render(request,'groups/create_group.html')
+
+class PiquedGroupViewSet(ModelViewSet):
+    serializer_class = PiquedGroupSerializer
+    queryset = PiquedGroup.objects.all()
+    permission_classes = [IsCreatable]
 
