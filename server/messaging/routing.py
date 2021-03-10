09cf59a0fe0ebb5cd165c7bd87bcfe0d5104d5af
jimmy <jimmychen12138@hotmail.com>
2021-03-10 13:22:00 +1100

    Removed pycache etc

--- server/messaging/routing.py
+++
@@ -1,8 +0,0 @@
-# messaging/routing.py
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/messaging/(?P<group_name>\w+)/$', consumers.GroupConsumer.as_asgi()),
-]
--- server/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-from django.db.models.fields import AutoField, DateField
-from django.db.models.fields.related import OneToOneField
-
-
-class PiquedUser(models.Model):
-    user: OneToOneField = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_of_birth: DateField = models.DateField("Date of Birth")
