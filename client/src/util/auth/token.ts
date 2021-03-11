efac24912ab3110cccc5d94f9fdcafe6868a72da
jimmy <jimmychen12138@hotmail.com>
2021-03-11 22:08:40 +1100

    Fixed token.ts

--- client/src/util/auth/token.ts
+++ client/src/util/auth/token.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { ACCESS_TOKEN } from "util/constants";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "util/constants";
 
 export const getAccessToken = () => localStorage.getItem(ACCESS_TOKEN);
 
 export const setAccessToken = (value: string) => localStorage.setItem(ACCESS_TOKEN, value);
 
 export const refreshAccessToken = async () => {
-  const token = localStorage.getItem("refreshToken");
-  const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/token/refresh`, { refresh: token });
+  const refresh_token = localStorage.getItem(REFRESH_TOKEN);
+  const res = await axios.post("/api/token/refresh", { refresh_token });
   return res.data.access_token;
 };
