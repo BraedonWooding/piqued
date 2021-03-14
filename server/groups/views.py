d60d3e8aa0316af287d23acf84df957bea769edd
Adam Temesvary <z5075774@student.unsw.edu.au>
2021-03-14 22:02:49 +1100

    groups changes in backend, may need to change groups model

--- server/groups/views.py
+++ server/groups/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import permissions
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 
 from .permission import IsCreatable
@@ -13,3 +14,11 @@ class PiquedGroupViewSet(ModelViewSet):
     queryset = PiquedGroup.objects.all()
     permission_classes = [permissions.IsAuthenticated]
     lookup_field = 'group_id'
+
+    @action(detail=True, methods=['delete'])
+    def remove_user(self, request, group_id):
+        piquedGroup = self.get_object()
+        userToRemove = self.request.user
+        userToRemove.groups.remove(piquedGroup.group.id)
+        userToRemove.save()
+        return HttpResponse("{} removed from group".format(userToRemove.username))      
