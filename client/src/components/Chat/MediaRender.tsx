ba1faf877113e463e3e7a93b8a35e76bc5cb8a1d
z5112968 <z5112968@ad.unsw.edu.au>
2021-03-17 21:02:45 +1100

    Just adding capital letter extensions to file formats

--- client/src/components/Chat/MediaRender.tsx
+++ client/src/components/Chat/MediaRender.tsx
@@ -8,8 +8,8 @@ interface MediaRenderProps {
 const MediaRender: FC<MediaRenderProps> = ({url, isRight}: MediaRenderProps ) => {
     const align = isRight ? "right" : "left";
     const justify = isRight ? "flex-end" : "flex-start"
-    var isImage:boolean = url.endsWith("png") || url.endsWith("jpg") || url.endsWith("jpeg") || url.endsWith("gif")
-    var isVideo:boolean = url.endsWith("mp4")
+    var isImage:boolean = url.endsWith("png") || url.endsWith("jpg") || url.endsWith("jpeg") || url.endsWith("gif") || url.endsWith("PNG") || url.endsWith("JPG") || url.endsWith("JPEG") || url.endsWith("GIF");
+    var isVideo:boolean = url.endsWith("mp4") || url.endsWith("MP4")
     if (isImage) {
         return (
             <div style={{textAlign: align, justifyContent: justify}}>
