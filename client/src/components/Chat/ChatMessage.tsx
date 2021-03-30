f077b673058bcded8b2f93628017ac2f1ac3816d
Braedon Wooding <braedonww@gmail.com>
2021-03-31 00:54:56 +1100

    [Chat]: Worked on a custom chat component, getting it rendered correctly now we just need to take it and mutate it into a real thing :D

--- client/src/components/Chat/ChatMessage.tsx
+++ client/src/components/Chat/ChatMessage.tsx
@@ -1,27 +1,69 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import PropTypes from "prop-types";
+import cx from "clsx";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
 
-interface ChatMessageProps {
-  position: string;
-  message: string;
+import withStyles from "@material-ui/core/styles/withStyles";
+import defaultChatMsgStyles from "@mui-treasury/styles/chatMsg/default";
+import { ChatMsg as ChatMsgType, Group, User } from "types";
+import { createMuiTheme, makeStyles } from "@material-ui/core";
+import { theme } from "theme";
+import { red } from "@material-ui/core/colors";
+
+interface ChatProps {
+  msgs: ChatMsgType[];
+  user: User;
+  side: "left" | "right";
 }
 
-export const ChatMessage: FC<ChatMessageProps> = ({ position = "right", message }) => {
-  const isRight = position.toLowerCase() === "right";
-  const align = isRight ? "text-right" : "text-left";
-  const justify = isRight ? "justify-content-end" : "justify-content-start";
-
+export const ChatMessage: FC<ChatProps> = ({ msgs, user, side }) => {
+  const classes = makeStyles(defaultChatMsgStyles)(createMuiTheme({
+    spacing: () => 2,
+    palette: {
+      background: {
+        default: "#F0F2F5",
+      },
+      primary: {
+        main: "#3578E5",
+      },
+      secondary: {
+        main: "#19857b",
+      },
+      error: {
+        main: red.A400,
+      },
+    }
+  }));
+  const attachClass = (index: number) => {
+    if (index === 0) {
+      return classes[`${side}First`];
+    }
+    if (index === msgs.length - 1) {
+      return classes[`${side}Last`];
+    }
+    return "";
+  };
   return (
-    <div className={`w-100 my-1 d-flex ${justify}`}>
-      <div className="bg-light rounded border border-gray p-2" style={{ maxWidth: "70%", flexGrow: 0 }}>
-        <span
-          className={`d-block text-secondary ${align}`}
-          style={{ fontWeight: 500, lineHeight: 1.4, whiteSpace: "pre-wrap" }}
-        >
-          {message}
-        </span>
-      </div>
-    </div>
+    <Grid container spacing={2} justify={side === "right" ? "flex-end" : "flex-start"}>
+      {side === "left" && (
+        <Grid item>
+          <Avatar src={user?.profile_picture || ""} className={classes.Avatar} />
+        </Grid>
+      )}
+      <Grid item xs={8}>
+        {msgs.filter(msg => msg.message).map((msg, i) => {
+          return (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={i} className={classes[`${side}Row`]}>
+              <Typography align={"left"} className={cx(classes.msg, classes[side], attachClass(i))}>
+                {msg.message}
+              </Typography>
+            </div>
+          );
+        })}
+      </Grid>
+    </Grid>
   );
 };
-
-export default ChatMessage;
--- server/groups/serializers.py
+++ server/groups/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
@@ -6,10 +7,29 @@ from user.models import PiquedUser
 from .models import Group, PiquedGroup
 
 
+class SimplifiedUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username')
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    email = serializers.EmailField(source='user.email')
+    profile_picture = serializers.ImageField(required=False, allow_null=True)
+    id = serializers.IntegerField(source='user.id', read_only=True)
+
+    class Meta:
+        model = PiquedUser
+        fields = ('date_of_birth', 'profile_picture', 'username', 'id',
+                  'email', 'first_name', 'last_name', 'interests', 'program', 'courses')
+
 class GroupSerializer(serializers.ModelSerializer):
+    user_set = serializers.SerializerMethodField()
+
+    def get_user_set(self, obj: Group):
+        users = PiquedUser.objects.filter(user_id__in = [u.id for u in obj.user_set.all()])
+        return [SimplifiedUserSerializer(pu).data for pu in users.all()]
+
     class Meta:
         model = Group
-        depth = 5
+        depth = 1
         fields = ('id', 'user_set', 'name')
 
 
