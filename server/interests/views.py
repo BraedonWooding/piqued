4ec37ccb1cdcab8d5ef41089da09a765e548f9a2
Nicholas Thumiger <nicholasthumiger@gmail.com>
2021-04-03 21:17:15 +1100

    added the facebook autofill

--- server/interests/views.py
+++ server/interests/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Interest
 from .serializers import InterestGraphSerializer, InterestSerializer
+from rest_framework.decorators import api_view
 
 
 class InterestViewSet(ModelViewSet):
@@ -13,3 +14,12 @@ class InterestGraphViewSet(ModelViewSet):
     # everyone can 'add' to the graph and modify stuff they own
     serializer_class = InterestGraphSerializer
     queryset = Interest.objects.all()
+
+@api_view(['POST'])
+def addInterests(request):
+    interests = request.data["interests"]
+
+    for i in interests:
+        print(i)
+
+    return Response({"status": "success"})
