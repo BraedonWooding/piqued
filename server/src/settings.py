8d3357afc15b8f0935a454b9408045462a9bc035
Braedon Wooding <braedonww@gmail.com.au>
2022-12-25 14:21:16 +1100

    Clean

--- server/src/settings.py
+++ server/src/settings.py
@@ -32,8 +32,7 @@ TABLE_STORAGE_CON_STRING = 'DefaultEndpointsProtocol=https;AccountName=piqued;Ac
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ***REMOVED***
-["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 mimetypes.add_type("text/css", ".css", True)
 mimetypes.add_type("text/html", ".html", True)
